ffd28b5f0eec9312651a176929dc65a9667bbc87
KamazRa4ka <thera4okml@gmail.com>
2023-11-17 20:47:00 +0100

    Added images to better teach players the game

--- commands/handlers/voice-handlers.js
+++ commands/handlers/voice-handlers.js
@@ -98,6 +98,7 @@ export const narrateAndPlayVoiceLine = async (client, guildId, channelId, voiceL
                     .setColor('3a3a3a')
                     .setTitle('Mafia Game')
                     .setDescription(`🎙 Bot: ${voiceLineText}`)
+                    .setImage('https://media.discordapp.net/attachments/1175130149516214472/1175153853440725123/introduction.png?ex=656a324f&is=6557bd4f&hm=dc9bfadab571050136b4ca51169c4ba85c161e6a10a5d5da02b805b6095bfa5c&=&width=1500&height=500')
                     .setTimestamp()
                     .setFooter({ text: 'MafiaBot', iconURL: 'https://media.discordapp.net/attachments/1148207741706440807/1174807401308901556/logo1500x1500.png?ex=6568efa7&is=65567aa7&hm=95d0bbc48ebe36cd31f0fbb418cbd406763a0295c78e62ace705c3d3838f823f&=&width=905&height=905' });
 
@@ -107,6 +108,79 @@ export const narrateAndPlayVoiceLine = async (client, guildId, channelId, voiceL
                         // Send a message to the channel
                         channel.send({ embeds: [embed] });
                     })
+
+                // wait 5 seconds and then play the next voice line
+                await setTimeout(async () => {
+                    const voiceLineText = 'So, let\'s start the game! I will explain the rules. First, you will be assigned a role. You can be a civilian, a mafia or a special role - Doctor, etc. The mafia\'s goal is to kill all the civilians.';
+                    narrateAndPlay(guildId, channelId, voiceLineText);
+
+                    const embed = new EmbedBuilder()
+                        .setColor('3a3a3a')
+                        .setTitle('Mafia Game')
+                        .setDescription(`🎙 Bot: ${voiceLineText}`)
+                        .setImage('https://media.discordapp.net/attachments/1175130149516214472/1175156913827217468/roles.png?ex=656a3529&is=6557c029&hm=19683650656d6faa043263f43b67045a580ec477c8e3f35f17952f1c5be080d1&=&width=1207&height=905')
+                        .setTimestamp()
+                        .setFooter({ text: 'MafiaBot', iconURL: 'https://media.discordapp.net/attachments/1148207741706440807/1174807401308901556/logo1500x1500.png?ex=6568efa7&is=65567aa7&hm=95d0bbc48ebe36cd31f0fbb418cbd406763a0295c78e62ace705c3d3838f823f&=&width=905&height=905' });
+
+                    client.channels.fetch(cId)
+                        .then(channel => {
+                            // Send a message to the channel
+                            channel.send({ embeds: [embed] });
+                        })
+                }, 15000);
+                await setTimeout(async () => {
+                    const voiceLineText = 'The goal of the game is to find out who is the mafia and kill them. The doctor can save one person each night. The detective can find out the role of one person each night.';
+                    narrateAndPlay(guildId, channelId, voiceLineText);
+
+                    const embed = new EmbedBuilder()
+                        .setColor('3a3a3a')
+                        .setTitle('Mafia Game')
+                        .setDescription(`🎙 Bot: ${voiceLineText}`)
+                        .setImage('https://media.discordapp.net/attachments/1175130149516214472/1175159008152268810/detective.png?ex=656a371c&is=6557c21c&hm=30348d795d2fde8aa8cd70fc4b57585b0bc7ea32b022dfb0314fa29a8dd38553&=&width=1207&height=905')
+                        .setTimestamp()
+                        .setFooter({ text: 'MafiaBot', iconURL: 'https://media.discordapp.net/attachments/1148207741706440807/1174807401308901556/logo1500x1500.png?ex=6568efa7&is=65567aa7&hm=95d0bbc48ebe36cd31f0fbb418cbd406763a0295c78e62ace705c3d3838f823f&=&width=905&height=905' });
+
+                    client.channels.fetch(cId)
+                        .then(channel => {
+                            // Send a message to the channel
+                            channel.send({ embeds: [embed] });
+                        })
+                }, 38000);
+                await setTimeout(async () => {
+                    const voiceLineText = 'The civilians can vote to kill one person each day. The mafia can kill one person each night. The game ends when all the mafia are dead, or when all the civilians are dead.';
+                    narrateAndPlay(guildId, channelId, voiceLineText);
+
+                    const embed = new EmbedBuilder()
+                        .setColor('3a3a3a')
+                        .setTitle('Mafia Game')
+                        .setDescription(`🎙 Bot: ${voiceLineText}`)
+                        .setImage('https://media.discordapp.net/attachments/1175130149516214472/1175159937303855154/civilian.png?ex=656a37fa&is=6557c2fa&hm=1f2b2e35b4f7a267fdacee9df6c338d26f648e8c617719ca6b90d7eab299027b&=&width=1207&height=905')
+                        .setTimestamp()
+                        .setFooter({ text: 'MafiaBot', iconURL: 'https://media.discordapp.net/attachments/1148207741706440807/1174807401308901556/logo1500x1500.png?ex=6568efa7&is=65567aa7&hm=95d0bbc48ebe36cd31f0fbb418cbd406763a0295c78e62ace705c3d3838f823f&=&width=905&height=905' });
+
+                    client.channels.fetch(cId)
+                        .then(channel => {
+                            // Send a message to the channel
+                            channel.send({ embeds: [embed] });
+                        })
+                }, 70000);
+                await setTimeout(async () => {
+                    const voiceLineText = ' A better instruction can be found in the chat. Good luck!';
+                    narrateAndPlay(guildId, channelId, voiceLineText);
+
+                    const embed = new EmbedBuilder()
+                        .setColor('3a3a3a')
+                        .setTitle('Mafia Game')
+                        .setDescription(`🎙 Bot: ${voiceLineText}`)
+                        .setTimestamp()
+                        .setFooter({ text: 'MafiaBot', iconURL: 'https://media.discordapp.net/attachments/1148207741706440807/1174807401308901556/logo1500x1500.png?ex=6568efa7&is=65567aa7&hm=95d0bbc48ebe36cd31f0fbb418cbd406763a0295c78e62ace705c3d3838f823f&=&width=905&height=905' });
+
+                    client.channels.fetch(cId)
+                        .then(channel => {
+                            // Send a message to the channel
+                            channel.send({ embeds: [embed] });
+                        })
+                }, 85000);
                 break;
             case '2':
                 narrateAndPlay(guildId, channelId, 'Hello, blyat, today we will play Mafia!');
