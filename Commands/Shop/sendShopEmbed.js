5626b8272d6695b3772be6661040123ef1e9184d
KamazRa4ka <thera4okml@gmail.com>
2023-11-29 18:42:05 +0100

    Changed design of the message

--- Commands/Shop/sendShopEmbed.js
+++ Commands/Shop/sendShopEmbed.js
@@ -4,17 +4,17 @@ import {getPlayerCoinsFromDatabase} from "../Handlers/DatabaseHandlers.js";
 export const sendShopEmbed = async (interaction) => {
     const item_1 = new ButtonBuilder()
         .setCustomId('buy_item_1')
-        .setLabel('💎 Active role | 🪙 1000 coins')
+        .setLabel('💎 Active role')
         .setStyle(ButtonStyle.Secondary);
 
     const item_2 = new ButtonBuilder()
         .setCustomId('buy_item_2')
-        .setLabel('🔎 Fake documents | 🪙 250 coins')
+        .setLabel('🔎 Fake documents')
         .setStyle(ButtonStyle.Secondary);
 
     const item_3 = new ButtonBuilder()
         .setCustomId('buy_item_3')
-        .setLabel('🔪 Knife | 🪙 1450 coins')
+        .setLabel('🔪 Knife')
         .setStyle(ButtonStyle.Secondary);
 
     const row = new ActionRowBuilder().addComponents(item_1).addComponents(item_2).addComponents(item_3);
@@ -26,9 +26,9 @@ export const sendShopEmbed = async (interaction) => {
         .setTitle('Mafia Game: Shop')
         .setDescription(`Welcome to the shop, ${interaction.user.username}! Here you can buy items that can help you later in the game. You cannot use items more than once. To earn coins, you have to win the game.`)
         .addFields(
-            { name: '💎 Active role', value: 'This item guarantees you an active role (Mafia, Doctor, etc) in the next game.', inline: true },
-            { name: '🔎 Fake documents', value: 'This item prevents detective from knowing your real role during a night check.', inline: true },
-            { name: '🔪 Knife', value: 'This item allows you to kill any person at night even if you are not a mafia.', inline: true }
+            { name: '💎 Active role', value: 'This item guarantees you an active role (Mafia, Doctor, etc) in the next game.\n  **🪙 1000 coins**', inline: true },
+            { name: '🔎 Fake documents', value: 'This item prevents detective from knowing your real role during a night check.\n  **🪙 250 coins**', inline: true },
+            { name: '🔪 Knife', value: 'This item allows you to kill any person at night even if you are not a mafia.\n  **🪙 1450 coins**', inline: true }
         )
         .setImage('https://media.discordapp.net/attachments/1174711985686970368/1179427879273103521/shop.png?ex=6579becf&is=656749cf&hm=76f671a50880fb8edf73e40d80c2bdc67ebf5d38468a324724dafcb970634048&=&format=webp&quality=lossless&width=1921&height=641')
         .setTimestamp()
