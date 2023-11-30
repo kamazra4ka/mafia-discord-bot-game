8a6e35ccb849a7fb33045b953d35709ce5ee368d
KamazRa4ka <thera4okml@gmail.com>
2023-11-30 12:36:34 +0100

    Added buying confirmation message termination timer

--- Commands/Shop/buyItem.js
+++ Commands/Shop/buyItem.js
@@ -1,21 +1,29 @@
-import {takeCoinsFromDatabase} from "../Handlers/DatabaseHandlers.js";
+import {addItemToUser, takeCoinsFromDatabase} from "../Handlers/DatabaseHandlers.js";
+import {editShopEmbed} from "./sendShopEmbed.js";
 
 export const buyItem = async (interaction, item) => {
+
     await takeCoinsFromDatabase(interaction.user.id, item);
-    let itemName;
-    // switch case to add emojis
-    switch (item) {
-        case 'activerole':
-            itemName = '💎 Active role';
-            break;
-        case 'fakedocuments':
-            itemName = '🔎 Fake documents';
-            break;
-        case 'knife':
-            itemName = '🔪 Knife';
-            break;
-        default:
-            itemName = 'Error';
-    }
-    await interaction.reply(`You bought **${itemName}**!`, { ephemeral: true });
+    await addItemToUser(interaction.user.id, item);
+
+        let itemName;
+        // switch case to add emojis
+        switch (item) {
+            case 'activerole':
+                itemName = '💎 Active role';
+                break;
+            case 'fakedocuments':
+                itemName = '🔎 Fake documents';
+                break;
+            case 'knife':
+                itemName = '🔪 Knife';
+                break;
+            default:
+                itemName = 'Error';
+        }
+        await interaction.reply({ content: `You bought **${itemName}**!`, ephemeral: true });
+
+        setTimeout(async () => {
+            await interaction.deleteReply();
+        }, 2500);
 }
