a9207ad2eb87c56b6530655351d191457a0ea565
KamazRa4ka <thera4okml@gmail.com>
2023-12-19 09:12:27 +0100

    Removed unused debug logging

--- Commands/shop.js
+++ Commands/shop.js
@@ -2,9 +2,6 @@ import {sendShopEmbed} from "./Shop/sendShopEmbed.js";
 import {checkUserInDatabaseItems} from "./Handlers/DatabaseHandlers.js";
 
 export const shop = async (interaction, client) => {
-
-    console.log(interaction.user.id)
-
     await checkUserInDatabaseItems(interaction.user.id);
     await sendShopEmbed(interaction, client)
 };
